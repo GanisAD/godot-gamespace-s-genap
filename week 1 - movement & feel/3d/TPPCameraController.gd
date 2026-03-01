62fa40d944138339a642312405dd17569b7c6fac
Syahrandy Waskito <syahrandywaskito666@gmail.com>
2026-03-01 20:18:02 +0700

    finish week 1 template project

--- week 1 - movement & feel/3d/TPPCameraController.gd
+++ week 1 - movement & feel/3d/TPPCameraController.gd
@@ -2,12 +2,18 @@ class_name TPPCameraController
 extends Node
 
 @export var camera_pivot: Node3D = null
+@export var camera: Camera3D = null
 @export_range(0.0, 1.0) var mouse_sensitivity: float = 0.25
 
 var _camera_input_direction: Vector2 = Vector2.ZERO
+var _forward: Vector3 = Vector3.ZERO
+var _right: Vector3 = Vector3.ZERO
 
 func _input(event: InputEvent) -> void:
-	pass
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
@@ -24,5 +30,14 @@ func _physics_process(delta: float) -> void:
 	# camera limit -PI/6.0 = -30 derajat | PI/3.0 = 60 derajat
 	camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -PI / 6.0, PI / 3.0) 
 	
-	camera_pivot.rotation.y = _camera_input_direction.x * delta
+	camera_pivot.rotation.y -= _camera_input_direction.x * delta
 	_camera_input_direction = Vector2.ZERO
+	
+	_forward = camera.global_basis.z
+	_right = camera.global_basis.x
+
+func get_forward() -> Vector3:
+	return _forward
+
+func get_right() -> Vector3:
+	return _right
