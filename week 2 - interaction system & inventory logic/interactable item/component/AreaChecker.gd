fe484adec8d3bd5f733da0f30228eafd8ef7525d
GanisAD <ganis.ahmad.com@gmail.com>
2026-04-02 13:06:52 +0700

    Creating lever scene

--- week 2 - interaction system & inventory logic/interactable item/component/AreaChecker.gd
+++ week 2 - interaction system & inventory logic/interactable item/component/AreaChecker.gd
@@ -14,9 +14,14 @@ func _ready() -> void:
 	body_exited.connect(_on_body_exit)
 
 func _on_body_enter(body: Node2D) -> void:
+	print("--- Sesuatu menyentuh area: ", body.name, " ---")
 	if body.is_in_group(group_name):
 		is_body_enter = true
+		print("DEBUG: ", body.name, " masuk! (Grup sesuai: ", group_name, ")")
+	else:
+		print("DEBUG: ", body.name, " masuk tapi diabaikan (Grup salah)")
 
 func _on_body_exit(body: Node2D) -> void:
 	if body.is_in_group(group_name):
 		is_body_enter = false
+		print("DEBUG: ", body.name, " keluar dari area.")
