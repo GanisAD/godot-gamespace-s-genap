35288c82bf633f9251fee542ca03a111a1a44ff0
GanisAD <ganis.ahmad.com@gmail.com>
2026-04-16 20:01:20 +0700

    Creating bullet knockback effect
    Creating knockback effect while bullet get in contact with entities or dummy (in this script at least)

--- week 3 - projectile & combat/2D/DummyTarget.gd
+++ week 3 - projectile & combat/2D/DummyTarget.gd
@@ -2,10 +2,13 @@ class_name DummyTarget
 extends CharacterBody2D
 
 @export var max_health: float = 100
+@export var friction: float = 10.0
+@export var gravity: float = 980.0
 
 @onready var health_bar: ProgressBar = $HealthBar
 
 var _current_health: float = 0
+var knockback_velocity: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	_current_health = max_health
@@ -17,5 +20,28 @@ func _process(_delta: float) -> void:
 	
 	health_bar.value = _current_health
 
-func take_damage(damage: float) -> void:
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if knockback_velocity != Vector2.ZERO:
+
+		knockback_velocity = knockback_velocity.lerp(Vector2.ZERO, friction * delta)
+		
+		if knockback_velocity.length() < 10:
+			knockback_velocity = Vector2.ZERO	   
+		
+		velocity.x = knockback_velocity.x
+		
+	move_and_slide()
+
+func take_damage(damage: float, k_force: float = 0.0, k_direction: Vector2 = Vector2.ZERO) -> void:
 	_current_health -= damage
+	
+	knockback_velocity = k_direction * k_force
+	if _current_health <= 0:
+		die()
+		
+func die() -> void:
+	queue_free()
--- week 3 - projectile & combat/2D/Projectile2D.gd
+++ week 3 - projectile & combat/2D/Projectile2D.gd
@@ -1,8 +1,9 @@
 class_name Projectile2D
 extends Area2D
 
-var projectile_speed: float = 200
-var projectile_damage: float = 10
+@export var projectile_speed: float = 200
+@export var projectile_damage: float = 10
+@export var knockback_force: float = 300.0
 
 func _ready() -> void:
 	body_entered.connect(_on_body_enter)
@@ -13,7 +14,7 @@ func set_projectile_speed(speed: float) -> void:
 func set_projectile_damage(damage: float) -> void:
 	projectile_damage = damage
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	global_position.x += projectile_speed * delta
 
 func _on_body_enter(body: Node2D) -> void:
@@ -22,6 +23,7 @@ func _on_body_enter(body: Node2D) -> void:
 	
 	if body.is_in_group("Enemy"):
 		if body.has_method("take_damage"):
-			body.take_damage(projectile_damage)
+			var knockback_dir = global_position.direction_to(body.global_position)
+			body.take_damage(projectile_damage, knockback_force, knockback_dir)
 			
 		queue_free()
